8b93d9853a0d1fd8983172fd505faee5be2bdafd
David Boureau <bdavidxyz@gmail.com>
2019-02-11 14:54:48 +0100

    cleaned

--- app/controllers/state_controller.rb
+++ app/controllers/state_controller.rb
@@ -3,10 +3,6 @@ class StateController < ApplicationController
   def create
     given_object = _params
     session[given_object["path"]] = YAML.load(given_object["model_and_state"])
-    p '- - - - - - - - - - - - - - session- - - - - - - - - - - - - - - -' 
-    pp given_object["path"]
-    pp session[given_object["path"]]
-    p ''
   end
 
   def _params
