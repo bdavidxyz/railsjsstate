8ad9bab56e7c187302c794f2f2465f7060cd76d2
David Boureau <bdavidxyz@gmail.com>
2019-02-11 10:30:26 +0100

    permit root param

--- app/controllers/hint_controller.rb
+++ app/controllers/hint_controller.rb
@@ -6,4 +6,13 @@ class HintController < ApplicationController
     }
   end
 
+  def before_unload
+    p "-----------------------------------------------------"
+    p _params
+  end
+
+  def _params
+    params.require(:main_store).permit!.to_h
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   get 'hint/index'
+  post 'hint/before_unload'
   
   root 'welcome#index'
 end
