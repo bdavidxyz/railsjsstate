65afacbd32318ec722d26ac9be358ed8310cd300
David Boureau <bdavidxyz@gmail.com>
2019-02-11 14:00:42 +0100

    first attempt of state restoration

--- app/services/hints_index.rb
+++ app/services/hints_index.rb
@@ -6,30 +6,27 @@ class HintsIndex
   end
 
   def call
-    current_path = GetCurrentPath.new(@request).call
-    pp "ap session[current_path]--------------------------------------------"
-    ap @session[current_path]
-    model_and_state = _default_model_and_state
-    # if _has_unchanged_model?(session)
-
+    model_and_state = _current
+    if _current["model"] == _previous["model"]
+      model_and_state = _previous
+    end
     return model_and_state
   end
 
-  def _has_unchanged_model?(previous_model)
-    # if (session[current_path])
-    _default_model_and_state("model")
+
+  def _previous
+    current_path = GetCurrentPath.new(@request).call
+    @session[current_path] ? @session[current_path]["model"] : {}
   end
 
-  def _default_model_and_state
-    model = {
-      hints: Hint.select(:name, :slug, :description).to_a.map{|e|e.attributes.compact!}
-    }
-    state = {
-      :active_tab => "1st"
-    }    
+  def _current
     return {
-      model:model,
-      state:state
+      "model" => {
+        "hints" => Hint.select(:name, :slug, :description).to_a.map{|e|e.attributes.compact!}
+      },
+      "state" => {
+        "active_tab" => "1st"
+      }
     }
   end
 
