cc8878b2f625935da2adb8d08bd91b6428714627
David Boureau <bdavidxyz@gmail.com>
2019-02-11 14:25:19 +0100

    hintjs uptodate

--- app/assets/javascripts/app/hint.js
+++ app/assets/javascripts/app/hint.js
@@ -21,13 +21,33 @@ $("document").ready(function(){
         newState.active_tab = action.with_slug
       }
 
-      localStorage.setItem(
-        'global_state_for_' + $("body").attr("data-path"), 
-        JSON.stringify({
-          model: gon.model_and_state["model"],
-          state:  newState,
-        }
-      ));
+      $.ajax({
+        type: "POST",
+        dataType: "application/json",
+        url: "/state/create",
+        headers: {
+         'X-CSRF-Token': document.querySelector("meta[name=csrf-token]").content
+        },
+        data: {
+          given_object: {
+            path: $("body").attr("data-path"),
+            model_and_state: JSON.stringify({
+              model: gon.model,
+              state: newState
+            })
+          }
+        },
+        success: function() {return false},
+        error: function() {return false},
+      });
+
+      // localStorage.setItem(
+      //   'global_state_for_' + $("body").attr("data-path"), 
+      //   JSON.stringify({
+      //     model: gon.model_and_state["model"],
+      //     state:  newState,
+      //   }
+      // ));
 
       return newState;
     };
